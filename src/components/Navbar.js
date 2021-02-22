879c738ba012338778b7b76315e359859b7cefa4
Kurt Choi <choi.kurt3@gmail.com>
2021-02-21 19:39:20 -0500

    add emphasis class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Signout from './Signout';
 
 const Navbar = () => {
-  return (  
+  return (
     <Flex direction="row" px="5px" align="center" h="50px" w="100%" bg="gray.300">
       <Text className="navbar-logo" fontFamily="Poppins" fontWeight="600" fontSize="lg" ml="10px">
-        Babble  
+        <i class="fas fa-comments emphasis"></i> Babble
       </Text>
       <Signout />
     </Flex>
   );
 }
- 
+
 export default Navbar;
